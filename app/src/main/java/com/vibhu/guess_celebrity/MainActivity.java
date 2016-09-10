5408f3abf3688371eccdc413057be1ebf1501cae
vibhudadhichi <vibhudadhikhi@gmail.com>
2016-09-11 01:17:01 +0530

    Getting Images Random Order

--- app/src/main/java/com/vibhu/guess_celebrity/MainActivity.java
+++ app/src/main/java/com/vibhu/guess_celebrity/MainActivity.java
@@ -1,16 +1,55 @@
 package com.vibhu.guess_celebrity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> celebURLs = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+    int chosenCeleb = 0;
+
+    ImageView imageView;
+
+    public class ImageDownloader extends AsyncTask<String,Void,Bitmap>{
+
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try{
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+
+                urlConnection.connect();
+                InputStream inputStream = urlConnection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+                return myBitmap;
+
+
+                }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
 
     public class DownloadTask extends AsyncTask<String,Void,String>{
 
@@ -54,13 +93,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = (ImageView) findViewById(R.id.imageView);
+
         DownloadTask task = new DownloadTask();
         String result = null;
 
         try {
             result = task.execute("https://www.posh24.com/celebrities").get();
 
-            
+            String[] splitResult = result.split("<div class=\"sidebarContainer\">");
+
+            Pattern p = Pattern.compile("src=\"(.*?)\"");
+            Matcher m = p.matcher(splitResult[0]);
+
+            while(m.find()){
+               celebURLs.add(m.group(1));
+            }
+
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
+
+            while (m.find()){
+                celebNames.add(m.group(1));
+            }
+
+            Random random = new Random();
+            chosenCeleb = random.nextInt(celebURLs.size());
+
+            ImageDownloader imageTask = new ImageDownloader();
+            Bitmap celebImage;
+
+            celebImage = imageTask.execute(celebURLs.get(chosenCeleb)).get();
+
+            imageView.setImageBitmap(celebImage);
+
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
