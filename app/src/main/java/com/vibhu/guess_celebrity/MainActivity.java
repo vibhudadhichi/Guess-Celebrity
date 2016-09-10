9e362afb906f2c5672b7ef965a95c8f9d748c6c0
vibhudadhichi <vibhudadhikhi@gmail.com>
2016-09-11 00:11:44 +0530

    Initial commit

---
+++ app/src/main/java/com/vibhu/guess_celebrity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vibhu.guess_celebrity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
